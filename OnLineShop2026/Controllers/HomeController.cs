fa10540b10758a8975148524815f095caad20171
Мария МД <mahadrum@yandex.ru>
2026-03-10 09:35:36 +0300

    Добавили карточку товара

--- OnLineShop2026/Controllers/HomeController.cs
+++ OnLineShop2026/Controllers/HomeController.cs
@@ -6,13 +6,7 @@ namespace OnLineShop2026.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+       
         public IActionResult Index()
         {
             return View();
@@ -23,10 +17,5 @@ namespace OnLineShop2026.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
