eb8b5e59ef97fe0c38f336290a10884240f005b7
яна кайтукова <yanakaytuk@gmail.com>
2026-03-30 14:32:30 +0300

    создан класс User

---
+++ OnLineShop2026/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace OnLineShop2026.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhotoPath { get; set; }
+
+        public User(string name, string email, string photoPath = "/images/ava.jpg")
+        {
+            Name = name;
+            Email = email;
+            PhotoPath = photoPath;
+        }
+
+        public override string ToString()
+        {
+            return $"Имя - {Name}, почта - {Email}";
+        }
+    }
+}
